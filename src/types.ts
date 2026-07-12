82852800070252668e9659700fe1d47729c34a33
Ramu Rajasekharan <mail@ramurajasekharan.com>
2026-07-13 00:10:09 +0530

    Restore types.ts, fix PageHeader optional title

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+export type Site = {
+  TITLE: string;
+  DESCRIPTION: string;
+  EMAIL: string;
+  NUM_POSTS_ON_HOMEPAGE: number;
+  NUM_PROJECTS_ON_HOMEPAGE: number;
+  NUM_BOOKS_ON_HOMEPAGE: number;
+};
+
+export type Metadata = {
+  TITLE: string;
+  DESCRIPTION: string;
+};
+
+export type Socials = {
+  NAME: string;
+  HREF: string;
+}[];
+
+export type Author = {
+  NAME: string;
+  TAGLINE: string;
+  BIO: string[];
+};
